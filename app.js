39fcf17f70a490a37272aed3098519b9c15ab9e0
Nouman Ejaz <itsnomi005@gmail.com>
2025-03-18 15:45:04 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`); // Make sure this line exists!
+});
+// Add this code to app.js
+app.get('/api/users', (req, res) => {
+  res.json([{ name: 'Alice', age: 25 }, { name: 'Bob', age: 30 }]);
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is the About page!');
+});
